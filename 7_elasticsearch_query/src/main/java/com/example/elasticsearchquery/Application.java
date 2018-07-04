43a7bf7897ec635e90d18a1ef6ff3ecb616a226b
wbb <zgrwbb@outlook.com>
2018-07-04 21:05:04 +0800

    7_elasticsearch_query

---
+++ 7_elasticsearch_query/src/main/java/com/example/elasticsearchquery/Application.java
@@ -0,0 +1,15 @@
+package com.example.elasticsearchquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangbobo
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
