f094e795f6cfd65a5737d83bd62caaeddfeedfca
wbb <zgrwbb@outlook.com>
2018-07-05 20:00:39 +0800

    8_mybatis_annotation

---
+++ 8_mybatis_annotation/src/main/java/com/example/mybatisannotation/Application.java
@@ -0,0 +1,15 @@
+package com.example.mybatisannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangbobo
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
