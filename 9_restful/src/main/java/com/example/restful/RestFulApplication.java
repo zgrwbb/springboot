c341f2077bb93601d4c176951e4686ae96c5f3d7
wbb <zgrwbb@outlook.com>
2018-07-19 21:45:15 +0800

    9_restful

---
+++ 9_restful/src/main/java/com/example/restful/RestFulApplication.java
@@ -0,0 +1,15 @@
+package com.example.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangbobo
+ */
+@SpringBootApplication
+public class RestFulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFulApplication.class, args);
+    }
+}
